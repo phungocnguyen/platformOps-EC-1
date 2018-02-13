c9074feb5c257b2d8f9955214788757d5cdd81c2
Phu Nguyen <phu.nguyen@cfpb.gov>
2018-02-13 16:33:42 -0500

    - Removed print debugger in ec_agent

--- ec_agent.go
+++ ec_agent.go
@@ -14,6 +14,7 @@ import (
 	"platformOps-EC/services"
 	"strings"
 	"time"
+	"net/http"
 )
 
 
@@ -33,66 +34,61 @@ func getECManifest(manifest string) []models.ECManifest {
 	return c
 }
 
+func getJsonManifestFromMaster(url string) []models.ECManifest {
 
+	var myClient = &http.Client{Timeout: 10 * time.Second}
 
-func main() {
+	resp, err := myClient.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	//decoder := json.NewDecoder(resp.Body)
+	//fmt.Println(decoder.Decode(&baseline))
 
-	fmt.Println("- Empowered by", models.ECVersion)
+	body, err := ioutil.ReadAll(resp.Body)
 
-	var input, output string
+	var baseline []models.ECManifest
 
-	flag.StringVar(&input, "i", "", "Input manifest json file. If missing, program will exit.")
-	flag.StringVar(&output, "o", "output.txt", "Execution output location.")
-	flag.Parse()
-
-	if input == "" {
-		fmt.Println("Missing input manifest. Program will exit.")
-		os.Exit(1)
+	if err != nil {
+		panic(err.Error())
 	}
 
-	if output == "output.txt" {
-		fmt.Println("Default to output.txt")
+	json.Unmarshal(body, &baseline)
+	return baseline
+}
 
-	}
+func CollectEvidence (baseline []models.ECManifest)  ([]models.ECManifestResult, []models.ECManifestResult)  {
 
-	var manifestResults []models.ECManifestResult
-
-	var manifestErrors []models.ECManifestResult
-
-	baseline := getECManifest(input)
-	if len(baseline) < 1 {
-		os.Exit(1)
-	}
-
-	fmt.Println("- Start executing commands")
-
+	var manifestResults, manifestErrors []models.ECManifestResult
 	for _, manifest := range baseline {
 		var b bytes.Buffer
-
+		var errorOutput string
+		s := ""
 		data := manifest.Command
+		if data != "" {
+			fmt.Printf("- Executing [%v]\n", manifest.Title)
 
-		fmt.Printf("- Executing [%v]\n", manifest.Title)
+			result := strings.Split(data, "|")
+			array := make([]*exec.Cmd, len(result))
+			for i := range result {
+				s := strings.TrimSpace(result[i])
+				commands := strings.Split(s, " ")
+				args := commands[1:len(commands)]
 
-		result := strings.Split(data, "|")
-		array := make([]*exec.Cmd, len(result))
-		for i := range result {
-			s := strings.TrimSpace(result[i])
-			commands := strings.Split(s, " ")
-			args := commands[1:len(commands)]
+				array[i] = exec.Command(commands[0], args...)
 
-			array[i] = exec.Command(commands[0], args...)
+			}
 
+			errorOutput = services.Execute(&b, array)
+
+			/*if err != nil {
+				log.Fatalln(err)
+			}
+			*/
+
+			s = b.String()
 		}
-
-		errorOutput := services.Execute(&b, array)
-
-		/*if err != nil {
-			log.Fatalln(err)
-		}
-		*/
-
-		s := b.String()
-
 		resultManifest := models.ECManifestResult{
 		                    models.ECManifest{manifest.ReqId,manifest.Title, manifest.Command, manifest.Baseline},
 			                s,
@@ -108,18 +104,8 @@ func main() {
 			manifestErrors = append(manifestErrors, errorManifest)
 		}
 
-
-
 	}
-
-	writeToFile(manifestResults, output)
-	fmt.Printf("- Done writing to [%v]\n", output)
-
-	if len(manifestErrors) > 0 {
-		errorFile := getErrorFileName(output)
-		writeToFile(manifestErrors, errorFile)
-		fmt.Printf("- Done writing error to [%v]\n", errorFile)
-	}
+	return manifestResults, manifestErrors
 }
 
 func writeToFile(baseline []models.ECManifestResult, output string) {
@@ -141,7 +127,6 @@ func writeToFile(baseline []models.ECManifestResult, output string) {
 		fmt.Fprintf(file, "\n%v\n", hashString)
 		fmt.Fprintf(file, "\n%v\n", baseline[i].Output)
 	}
-
 }
 
 func dateTimeNow() string {
@@ -152,3 +137,52 @@ func getErrorFileName(output string) string{
 	return filepath.Join(filepath.Dir(output), "error_"+filepath.Base(output))
 }
 
+
+func main() {
+
+	fmt.Println("- Empowered by", models.ECVersion)
+
+	var input, output, mode string
+
+	flag.StringVar(&input,  "i", "", "Input manifest json file. If missing, program will exit.")
+	flag.StringVar(&output, "o", "output.txt", "Execution output location.")
+	flag.StringVar(&mode,   "m", "local", "Run as Web agent or local CLI agent. -m w as Web agent. Default local CLI agent. ")
+	flag.Parse()
+
+	if input == "" {
+		fmt.Println("Missing input manifest. Program will exit.")
+		os.Exit(1)
+	}
+
+	if output == "output.txt" {
+		fmt.Println("Default to output.txt")
+
+	}
+
+	// populate baseline by requesting manifest from master
+	// or input manifest at command line argument.
+	var baseline []models.ECManifest
+	if mode == "local" {
+		baseline = getECManifest(input)
+	} else {
+		baseline = getJsonManifestFromMaster(input)
+	}
+	if len(baseline) < 1 {
+		os.Exit(1)
+	}
+
+	fmt.Println("- Start executing commands")
+	//collecting evidence by executing commands
+	manifestResults, manifestErrors := CollectEvidence(baseline)
+
+	// write result to output file
+	writeToFile(manifestResults, output)
+	fmt.Printf("- Done writing to [%v]\n", output)
+
+	// write error to error output file
+	if len(manifestErrors) > 0 {
+		errorFile := getErrorFileName(output)
+		writeToFile(manifestErrors, errorFile)
+		fmt.Printf("- Done writing error to [%v]\n", errorFile)
+	}
+}
